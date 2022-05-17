94c1e5763db930e3a237d2dfc3498a0e1e2042fe
jose cru <jose227716@gmail.com>
2022-05-16 20:16:21 -0400

    update.py

--- TheDreamProject/vision_app/urls.py
+++ TheDreamProject/vision_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from vision_app.views import Homepage, Addvision
+from vision_app.views import Homepage, Addvision,VisionListView
 
 urlpatterns = [
     path('', Homepage.as_view(), name='Home'),
-    path('addvision/', Addvision.as_view(), name='AddVision')
+    path('addvision/', Addvision.as_view(), name='AddVision'),
+    path('Dashboard/', VisionListView.as_view(), name='article-list')
 ]
+
--- TheDreamProject/vision_app/views.py
+++ TheDreamProject/vision_app/views.py
@@ -2,7 +2,22 @@
 # Create your views here.
 from django.shortcuts import render
 from django.views import View
-from django.views import Task
+#from django.views import Task
+from vision_app.models import Vision
+from django.views.generic.list import ListView
+
+class VisionListView(ListView):
+
+    model = Vision
+    paginate_by = 100  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+      #  context['now'] = timezone.now()
+        return context
+
+        
+
 
 
 # Create your views here.
