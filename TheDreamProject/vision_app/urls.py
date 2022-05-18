bbc764d13396edad1b1a78e5bb8d39fd12fdb569
Harbir01 <hsingh017@outlook.com>
2022-05-17 20:58:17 -0400

    Update Database, Add Goals,Task classes

--- TheDreamProject/vision_app/urls.py
+++ TheDreamProject/vision_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from vision_app.views import Homepage, Addvision,VisionListView
+from vision_app.views import Homepage, Addvision,VisionListView,GoalListView,TaskListView
 
 urlpatterns = [
     path('', Homepage.as_view(), name='Home'),
     path('addvision/', Addvision.as_view(), name='AddVision'),
-    path('Dashboard/', VisionListView.as_view(), name='article-list')
+    path('Dashboard/', VisionListView.as_view()),
+    path('goal/', GoalListView.as_view()),
+    path('task/', TaskListView.as_view()),
 ]
 
