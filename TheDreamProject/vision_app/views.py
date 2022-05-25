a8564e839719217cd848e2b4bb77441c586ddb51
Harbir01 <hsingh017@outlook.com>
2022-05-25 16:24:12 -0400

    Update Post method in views file

--- TheDreamProject/vision_app/views.py
+++ TheDreamProject/vision_app/views.py
@@ -56,4 +56,16 @@ class Addvision(View):
 
 class AddGoalTask(View): 
     def get(self, request):
-        return render(request, 'addgoaltask.html')
+        return render(request, 'addgoaltask.html',{
+                '   Goal_Form' : Goal_Form,
+            },{
+                'Task_Form' : Task_Form,
+            })
+    
+    def post(self,request):
+        '''POST the data in the form submitted by the user, creating a new task in the todo list'''
+        Goal_Form = GoalForm(request.POST)
+        Goal_Form.save()
+        Task_Form = TaskForm(request.POST)
+        Task_Form.save()
+        return render(request,'addgoaltask.html')
