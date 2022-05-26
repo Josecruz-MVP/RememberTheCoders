9589c5678c8ae80b62e80cde36e829dca0f6c0a2
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-05-26 19:27:36 -0400

    updates

--- TheDreamProject/vision_app/views.py
+++ TheDreamProject/vision_app/views.py
@@ -43,34 +43,55 @@ class Homepage(View):
     def get(self, request):
         return render(request, 'home.html')
 
+'''Vision'''
 class Addvision(View):
     def get(self, request):
         Vision_Form = VisionForm()
+        
         return render(request, 'add_vision.html',{
                 'Vision_Form' : Vision_Form,
             })
+
+            
     def post(self,request):
         '''POST the data in the form submitted by the user, creating a new task in the todo list'''
         Vision_Form = VisionForm(request.POST)
         Vision_Form.save()
         return render(request,'add_vision.html')
 
+'''Goal'''
 class AddGoalTask(View): 
     def get(self, request):
         Goal_Form = GoalForm()
-        Task_Form = TaskForm()
+        
         return render(request, 'addgoaltask.html',{
                 '   Goal_Form' : Goal_Form,
-            },{
-                'Task_Form' : Task_Form,
             })
     
     def post(self,request):
         '''POST the data in the form submitted by the user, creating a new task in the todo list'''
         Goal_Form = GoalForm(request.POST)
         Goal_Form.save()
-        Task_Form = TaskForm(request.POST)
-        Task_Form.save()
+        
         return render(request,'addgoaltask.html')
+
+
+'''Task'''
+class AddGoalTask(View): 
+    def get(self, request):
+        Task_Form = TaskForm()
+        
+        return render(request, 'addgoaltask.html',{
+                '   Task_Form' : Task_Form,
+            })
+    
+    def post(self,request):
+        '''POST the data in the form submitted by the user, creating a new task in the todo list'''
+        Goal_Form = GoalForm(request.POST)
+        Goal_Form.save()
+        
+        return render(request,'addgoaltask.html')
+
+
 
 
