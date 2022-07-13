af99a1f2431965f6a96549deb2d8cef90311bd1e
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-07-13 16:10:46 -0400

    updates

--- TheDreamProject/vision_app/urls.py
+++ TheDreamProject/vision_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 
-from vision_app.views import Homepage, Addvision, AddGoalTask, VisionListView
+from vision_app.views import Homepage, Addvision, AddGoalTask, VisionListView, GoalListView
 
 urlpatterns = [
     path('', Homepage.as_view(), name='Home'),
     path('addvision/', Addvision.as_view(), name='AddVision'),
-    path('addgoaltask/', AddGoalTask.as_view(), name='AddGoalTask'),
-    path('addgoaltask/', VisionListView.as_view(), name='add_vision')
+    path('addgoaltask/', AddGoalTask.as_view(), name='addgoaltaskview'),
+    path('listvision/', VisionListView.as_view(), name='add_vision'),
+    path('dashboard/', GoalListView.as_view(), name='dashboard')
 
 ]
 
